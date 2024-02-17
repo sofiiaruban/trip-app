a636f050048461e814e00d71518a3a6d889184e1
sofiiaruban <sophiaruban249324@gmail.com>
2024-02-17 20:37:51 +0200

    feature(Main page): Add snapshot test

--- src/pages/__test__/Main.test.tsx
+++ src/pages/__test__/Main.test.tsx
@@ -2,11 +2,7 @@ import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import Main from '../Main'
 
-test('demo', () => {
-  expect(true).toBe(true)
-})
-
 test('Renders the main page', () => {
-  render(<Main />)
-  expect(true).toBeTruthy()
+  const { container } = render(<Main />)
+  expect(container).toMatchSnapshot()
 })
