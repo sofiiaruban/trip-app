57b11526b6319017f40c3b27bcdc867d309ac52a
sofiiaruban <sophiaruban249324@gmail.com>
2024-02-25 17:30:21 +0200

    fx img src

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export enum CityList {
 }
 
 export enum CityImg {
-  NEW_YORK = 'trip-app/src/assets/newyork.jpeg',
-  LONDON = 'trip-app/src/assets/london.jpg',
-  KYIV = 'trip-app/src/assets/kyiv.jpg'
+  NEW_YORK = 'assets/newyork.jpeg',
+  LONDON = 'assets/london.jpg',
+  KYIV = 'assets/kyiv.jpg'
 }
